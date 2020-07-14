7eba47c541f44c00a7dadbaf613dd90055b856b5
Viacheslav Slinko <vslinko@yahoo.com>
2020-07-14 17:52:23 +0300

    Upgrade to Deno 1.2.0 and stdlib 0.61.0

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,5 +1,5 @@
 export {
   assertEquals,
   assertThrowsAsync,
-} from "https://deno.land/std@0.52.0/testing/asserts.ts";
-export * as log from "https://deno.land/std@0.52.0/log/mod.ts";
+} from "https://deno.land/std@0.61.0/testing/asserts.ts";
+export * as log from "https://deno.land/std@0.61.0/log/mod.ts";
