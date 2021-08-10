7f3985ef28f75ca38405f7a2e00abbd2b4fcc7ac
op-personal <73725583+op-personal@users.noreply.github.com>
2021-08-10 13:49:36 -0400

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -2,6 +2,6 @@ export {
   concat,
   findIndex,
   repeat,
-} from "https://deno.land/std@0.75.0/bytes/mod.ts";
-export { getLogger } from "https://deno.land/std@0.75.0/log/mod.ts";
-export { Logger } from "https://deno.land/std@0.75.0/log/logger.ts";
+} from "https://deno.land/std@0.104.0/bytes/mod.ts";
+export { getLogger } from "https://deno.land/std@0.104.0/log/mod.ts";
+export { Logger } from "https://deno.land/std@0.104.0/log/logger.ts";
