d9a5c6d8e55095931ff8d2c34f1eb1487cfa1bf4
op-personal <73725583+op-personal@users.noreply.github.com>
2021-08-10 13:53:58 -0400

    Update dev_deps.ts

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,5 +1,5 @@
 export {
   assertEquals,
   assertThrowsAsync,
-} from "https://deno.land/std@0.75.0/testing/asserts.ts";
-export * as log from "https://deno.land/std@0.75.0/log/mod.ts";
+} from "https://deno.land/std@0.104.0/testing/asserts.ts";
+export * as log from "https://deno.land/std@0.104.0/log/mod.ts";
