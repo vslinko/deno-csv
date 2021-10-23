e58a3761d5e99799abd4c6d4d5910d769f19163c
Viacheslav Slinko <vslinko@yahoo.com>
2021-10-23 16:00:56 +0300

    Update deno std to 0.112.0

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ export {
   concat,
   indexOf,
   repeat,
-} from "https://deno.land/std@0.104.0/bytes/mod.ts";
-export { getLogger } from "https://deno.land/std@0.104.0/log/mod.ts";
-export { Logger } from "https://deno.land/std@0.104.0/log/logger.ts";
-export { iter } from "https://deno.land/std@0.104.0/io/util.ts";
+} from "https://deno.land/std@0.112.0/bytes/mod.ts";
+export { getLogger } from "https://deno.land/std@0.112.0/log/mod.ts";
+export { Logger } from "https://deno.land/std@0.112.0/log/logger.ts";
+export { iter } from "https://deno.land/std@0.112.0/io/util.ts";
--- dev_deps.ts
+++ dev_deps.ts
@@ -1,6 +1,6 @@
 export {
   assertEquals,
   assertThrowsAsync,
-} from "https://deno.land/std@0.104.0/testing/asserts.ts";
-export * as log from "https://deno.land/std@0.104.0/log/mod.ts";
-export { Buffer } from "https://deno.land/std@0.104.0/io/buffer.ts";
+} from "https://deno.land/std@0.112.0/testing/asserts.ts";
+export * as log from "https://deno.land/std@0.112.0/log/mod.ts";
+export { Buffer } from "https://deno.land/std@0.112.0/io/buffer.ts";
