6d994b158a1257e6d825b1a715ed17819ff62422
Viacheslav Slinko <me@vslinko.com>
2023-08-01 12:05:16 +0300

    update to deno 1.35.3 and std 0.195.0

--- deps.ts
+++ deps.ts
@@ -2,6 +2,6 @@ export {
   concat,
   indexOfNeedle,
   repeat,
-} from "https://deno.land/std@0.164.0/bytes/mod.ts";
-export { getLogger, Logger } from "https://deno.land/std@0.164.0/log/mod.ts";
-export { iterateReader } from "https://deno.land/std@0.164.0/streams/conversion.ts";
+} from "https://deno.land/std@0.195.0/bytes/mod.ts";
+export { getLogger, Logger } from "https://deno.land/std@0.195.0/log/mod.ts";
+export { iterateReader } from "https://deno.land/std@0.195.0/streams/iterate_reader.ts";
--- dev_deps.ts
+++ dev_deps.ts
@@ -1,7 +1,7 @@
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.164.0/testing/asserts.ts";
-export * as log from "https://deno.land/std@0.164.0/log/mod.ts";
-export { Buffer } from "https://deno.land/std@0.164.0/io/buffer.ts";
-export { SHA256 as Sha256 } from "https://denopkg.com/chiefbiiko/sha256@v1.0.2/mod.ts";
+} from "https://deno.land/std@0.195.0/testing/asserts.ts";
+export * as log from "https://deno.land/std@0.195.0/log/mod.ts";
+export { Buffer } from "https://deno.land/std@0.195.0/io/buffer.ts";
+export { SHA256 as Sha256 } from "https://denopkg.com/chiefbiiko/sha256@v2.0.0/mod.ts";
